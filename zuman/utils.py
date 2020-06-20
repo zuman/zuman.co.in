eb6275b3a463a8c19e047508ff437e44a51b52c2
Zap Hap <zaphap01@gmail.com>
2020-06-20 19:11:53 +0000

    auto logout on browser change

--- zuman/utils.py
+++ zuman/utils.py
@@ -5,14 +5,14 @@ from flask_login import logout_user
 def validate_session():
     if 'HTTP_USER_AGENT' not in session:
         set_session()
-    if session['HTTP_USER_AGENT'] != request.environ['HTTP_USER_AGENT'] or session['REMOTE_ADDR'] != request.environ['REMOTE_ADDR']:
+        return
+    if session['HTTP_USER_AGENT'] != request.environ['HTTP_USER_AGENT']:
         logout()
         abort(409)
 
 
 def set_session():
     session['HTTP_USER_AGENT'] = request.environ['HTTP_USER_AGENT']
-    session['REMOTE_ADDR'] = request.environ['REMOTE_ADDR']
 
 
 def logout():
