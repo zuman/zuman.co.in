07aadd2816a9282cfae4c75b40c9da433625a8dd
Zap Hap <syed.zuman.007@gmail.com>
2020-06-21 09:57:44 +0000

    user authentication check

--- zuman/utils.py
+++ zuman/utils.py
@@ -6,9 +6,9 @@ from zuman import appdata
 def validate_session():
     if 'HTTP_USER_AGENT' not in session:
         set_session()
-    if current_user.id not in appdata['user_sessions']:
+    if current_user.is_authenticated and (current_user.id not in appdata['user_sessions']):
         appdata['user_sessions'][current_user.id] = session.sid
-    if appdata['user_sessions'][current_user.id] != session.sid or session['HTTP_USER_AGENT'] != request.environ['HTTP_USER_AGENT']:
+    if (current_user.is_authenticated and (appdata['user_sessions'][current_user.id] != session.sid)) or session['HTTP_USER_AGENT'] != request.environ['HTTP_USER_AGENT']:
         logout()
         flash("Login detected from another browser or device.", "danger")
         flash("Please login again to continue using the site here!", "danger")
