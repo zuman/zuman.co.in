38c206e54c6f39b3d6a3b44b020ccdf6460b1229
Alien Zap <zaphap01@gmail.com>
2020-06-21 18:09:01 +0530

    Fix single user login

--- zuman/utils.py
+++ zuman/utils.py
@@ -1,14 +1,17 @@
 from flask import request, session, flash, redirect, url_for, abort
 from flask_login import current_user, logout_user
 from zuman import appdata
+import uuid
 
 
 def validate_session():
-    if 'HTTP_USER_AGENT' not in session:
-        set_session()
-    if current_user.is_authenticated and (current_user.id not in appdata['user_sessions']):
+    if not current_user.is_authenticated:
+        return
+    if current_user.id not in appdata['user_sessions']:
         appdata['user_sessions'][current_user.id] = session.sid
-    if (current_user.is_authenticated and (appdata['user_sessions'][current_user.id] != session.sid)) or session['HTTP_USER_AGENT'] != request.environ['HTTP_USER_AGENT']:
+    if (appdata['user_sessions'][current_user.id] != session.sid)\
+            or ('HTTP_USER_AGENT' not in session)\
+            or (session['HTTP_USER_AGENT'] != request.environ['HTTP_USER_AGENT']):
         logout()
         flash("Login detected from another browser or device.", "danger")
         flash("Please login again to continue using the site here!", "danger")
@@ -16,6 +19,8 @@ def validate_session():
 
 
 def set_session():
+    session.sid = str(uuid.uuid4())
+    appdata['user_sessions'][current_user.id] = session.sid
     session['HTTP_USER_AGENT'] = request.environ['HTTP_USER_AGENT']
 
 
